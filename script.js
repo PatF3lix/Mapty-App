c71bdf492a22ee6ad0f6d0d2e03f1027d0fbc34b
PatF3lix <pat_pg@hotmail.fr>
2023-11-03 11:04:41 +0000

    added Map to application, third party library leaflet combined with navigator.geolocation API.

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const map = L.map('map').setView([latitude, longitude], 15);
+
+      /**The map is made out of titles */
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('Current Location')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
