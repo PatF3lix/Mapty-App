52b12019cabd40b863eb93e43e6bf391d544976e
PatF3lix <pat_pg@hotmail.fr>
2023-11-03 14:45:41 +0000

    refactored the application part 2, inserted event listeners inside constructor and used bind() to enable the this keyword during regular function calls.

--- script.js
+++ script.js
@@ -11,33 +11,36 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-/**State Variables */
-let map;
-let mapEvent;
-
 class MaptyApp {
+  #map;
+  #mapEvent;
   constructor() {
     this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
   }
   _getPosition() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this._loadMap, function () {
-        alert('Could not get your position');
-      });
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
     }
   }
 
   _loadMap(position) {
     const { latitude, longitude } = position.coords;
-    map = L.map('map').setView([latitude, longitude], 15);
+    this.#map = L.map('map').setView([latitude, longitude], 15);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
     L.marker([latitude, longitude])
-      .addTo(map)
+      .addTo(this.#map)
       .bindPopup('Current Location', {
         maxWidth: 250,
         minWidth: 100,
@@ -48,19 +51,26 @@ class MaptyApp {
       .openPopup();
 
     /**Handling clicks on map */
-    map.on('click', function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-    });
+    this.#map.on('click', this._showForm.bind(this));
   }
 
-  _showForm(e) {
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
     /**Display marker */
     e.preventDefault();
-    const { lat, lng } = mapEvent.latlng;
+    const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
-      .addTo(map)
+      .addTo(this.#map)
       .bindPopup('Workout', {
         maxWidth: 250,
         minWidth: 100,
@@ -78,23 +88,7 @@ class MaptyApp {
         '';
     inputDistance.focus();
   }
-
-  _toggleElevationField() {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-  }
-
-  _newWorkout() {}
 }
 
 /**Start application */
 const app = new MaptyApp();
-
-form.addEventListener('submit', function (e) {
-  app._showForm(e);
-});
-
-/**change between forms running and cycling */
-inputType.addEventListener('change', function () {
-  app._toggleElevationField();
-});
